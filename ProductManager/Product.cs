13d9bc056a6a316ec3e16392a3c5575c2f924f90
aiden-richard <richardaiden2006@gmail.com>
2025-05-22 19:11:09 -0700

    Added a Product class

---
+++ ProductManager/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager
+{
+    /// <summary>
+    /// Represents a product for sale
+    /// </summary>
+    internal class Product
+    {
+        /// <summary>
+        /// A unique identifier for the product
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The user facing name of the product
+        /// </summary>
+        public required string Name { get; set; }
+    }
+}
