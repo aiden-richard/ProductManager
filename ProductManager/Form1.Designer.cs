85a4ffb5cf862f2b8c3514e4f2b77c8d074701ed
aiden-richard <richardaiden2006@gmail.com>
2025-05-22 18:28:27 -0700

    Added UI components to add a product for the main form

--- ProductManager/Form1.Designer.cs
+++ ProductManager/Form1.Designer.cs
@@ -28,19 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            txt = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Product Name";
+            // 
+            // txt
+            // 
+            txt.Location = new Point(149, 66);
+            txt.Name = "txt";
+            txt.Size = new Size(100, 23);
+            txt.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(121, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 61);
+            button1.TabIndex = 2;
+            button1.Text = "Add Product";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(570, 469);
+            ClientSize = new Size(346, 309);
+            Controls.Add(button1);
+            Controls.Add(txt);
+            Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Product Manager";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txt;
+        private Button button1;
     }
 }
